6076b31f35af1713d848d761352d453428fdc807
jduval23 <48337343+jduval23@users.noreply.github.com>
2020-07-21 08:58:03 -0700

    removed redundant check from some tests

--- jsLibrary/tests/test_audio-mode.js
+++ jsLibrary/tests/test_audio-mode.js
@@ -23,14 +23,7 @@ let library;
 
 describe('test_audio-mode', () => {
     before(() => {
-        library = new rokuLibrary.Library("192.168.1.64");
-    });
-
-    it('Check if channel exist on the device', async function() { 
-        this.timeout(5000);
-        const apps = await library.getApps();
-        const res = library.verifyIsChannelExist(apps, 'dev');
-        expect(res).equal(true);
+        library = new rokuLibrary.Library("192.168.2.11");
     });
 
     it('should launch the channel', async function() { 
