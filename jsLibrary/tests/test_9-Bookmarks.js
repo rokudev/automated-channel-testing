9211eb4ae7130b9c4a75172197a1dea2ba2591d6
jduval23 <48337343+jduval23@users.noreply.github.com>
2020-07-21 09:00:59 -0700

    removed redundant check from some tests

--- jsLibrary/tests/test_9-Bookmarks.js
+++ jsLibrary/tests/test_9-Bookmarks.js
@@ -23,14 +23,7 @@ let library;
 
 describe('test_9-Bookmarks', () => {
     before(() => {
-        library = new rokuLibrary.Library("192.168.2.121");
-    });
-
-    it('Check if channel exist on the device', async function() { 
-        this.timeout(5000);
-        const apps = await library.getApps();
-        const res = library.verifyIsChannelExist(apps, 'dev');
-        expect(res).equal(true);
+        library = new rokuLibrary.Library("192.168.2.11");
     });
 
     it('should launch the channel', async function() { 
@@ -70,4 +63,3 @@ describe('test_9-Bookmarks', () => {
         childProcess.kill();
     });
 });
-
